9b32d0db5a6571a66e1a0cbd481e6e4aa70a3624
PAUPAUSAZON <pausazon09@gmail.com>
2026-08-13 16:21:36 +0800

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+import math
+
+print("---Task 1:Angle Explorer---")
+angle_degrees = 60
+angle_radians = math.radians(angle_degrees)
+print(f"Angle in radians: {angle_radians:.4f}")
+print(f"sin -> {math.sin(angle_radians):.4f}")
+print(f"cos -> {math.cos(angle_radians):.4f}")
+print(f"tan -> {math.tan(angle_radians):.4f}")
+
+print()
---
+++ Task2.py
@@ -0,0 +1,9 @@
+import math
+
+print("---Task 2: Right Triangle Solver")
+leg_a = 9
+leg_b = 12
+hypotenuse = math.hypot(9, 12)
+print(f"Hypotenuse: {hypotenuse}")
+
+print()
