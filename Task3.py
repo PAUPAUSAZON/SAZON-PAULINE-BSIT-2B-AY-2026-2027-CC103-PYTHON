c9500729c7db8b52531af747b30180f102b7e832
PAUPAUSAZON <pausazon09@gmail.com>
2026-08-13 16:32:22 +0800

    Add files via upload

---
+++ Task3.py
@@ -0,0 +1,11 @@
+import math
+
+print("---Task 3: Rounding Showdown---")
+value = -7.8
+print(f"ceil -> {math.ceil(value)}")
+print(f"floor -> {math.floor(value)}")
+print(f"trunc -> {math.trunc(value)}")
+
+#floor finds the integer that is less than the number, trunc removes the decimal portion completely. 
+
+print()
